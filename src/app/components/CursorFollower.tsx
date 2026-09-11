38dbf0be1da16937e66a961f4cb83e9483cf0ec4
Himanshu Dubey <whyhimanshu777@gmail.com>
2026-09-11 13:55:40 +0530

    fix the problam

--- src/app/components/CursorFollower.tsx
+++ src/app/components/CursorFollower.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import  useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function CursorFollower() {
   const dotRef = useRef<HTMLDivElement>(null);
